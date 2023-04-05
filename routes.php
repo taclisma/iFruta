2305f911280cfcd1fba890c10014fa15c322b661
taclisma <marianafe00@gmail.com>
2023-04-05 09:06:40 -0300

    config php deleted

--- routes.php
+++ routes.php
@@ -4,9 +4,10 @@
 $router->get('/', 'controllers/index.php')->only('guest');
 
 $router->post('/auth', 'controllers/auth.php');
-$router->get('/auth', 'controllers/auth.php')->only('auth');
+$router->get('/auth', 'controllers/auth.php')->only('guest');
 
 $router->get('/registro', 'controllers/registro.php')->only('auth');
+
 $router->post('/registrar', 'controllers/registrar.php')->only('auth');
 
 $router->get('/exit', 'controllers/exit.php');
