3017e8ebbfeb9fc00b9bf6b0643c2380bab8b45d
taclisma <marianafe00@gmail.com>
2023-05-16 10:20:19 -0300

    rename public

--- controllers/index.php
+++ controllers/index.php
@@ -1,11 +1,4 @@
 <?php
 
 
-// // funciona, é seguro?
-// if (isset($_SESSION['current_user']) && !empty($_SESSION['current_user'])){
-//     // fazer isso funcionar com novo redicionamento do auth com o mostrar erro
-//     session_destroy();
-
-// }    
-
 view('index.view.php');
--- controllers/registrar.php
+++ controllers/registrar.php
@@ -15,6 +15,10 @@ $db->query('INSERT INTO REGISTROS (id_aluno) VALUES(:curr_user);', ['curr_user' 
 $data_reg = $db->query('SELECT registro from REGISTROS where id_aluno = :user && cast(registro as Date) = cast(current_timestamp() as Date);', ['user' => $_POST['user']])->fetchOrFail(Response::FORBIDDEN);
 
 $_SESSION['data_reg'] = $data_reg['registro'];
+$_SESSION['status'] = 'feito';
 
-//pag animação, passa data de reg pra view
+// redireciona para pag animação
+header('location: /registro');
+
+exit();
 view('feito.view.php', ['status' => 'feito', 'data' => $_SESSION['data_reg']]);
